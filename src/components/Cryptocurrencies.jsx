3ab6f7d5c20a648ae8b8abbafd408d46b02c319e
lukeryandev <lukeryan93@pm.me>
2021-09-20 19:52:07 +0100

    Updated build errors

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -40,7 +40,7 @@ const Cryptocurrencies = ({ simplified }) => {
             <Link key={currency.id} to={`/crypto/${currency.id}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
-                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                extra={<img className="crypto-image" src={currency.iconUrl} alt="icon" />}
                 hoverable
               >
                 <p>Price: {millify(currency.price)}</p>
